d3a7bc9785c36baca554e5f87563bfdfe4c2c393
erikbayerlein <100382086+erikbayerlein@users.noreply.github.com>
2022-05-05 11:18:04 -0300

    Add files via upload

--- salario.py
+++ salario.py
@@ -1,8 +1,22 @@
-print ("Vamos calcular seu salário mensal.")
+print ('Responda as seguintes respostas com 1 para sim e 0 para nao')
 
-horas = float(input("Digite a quantidade de horas que você trabalhou: "))
-din = float(input("Digite o valor que recebe por hora: "))
+p1 = int(input('Telefonou para a vítima?'))
+p2 = int(input('Esteve no local do críme?'))
+p3 = int(input('Mora perto da vítima?'))
+p4 = int(input('Devia para a vítima?'))
+p5 = int(input('Já trabalhou com a vítima?'))
 
-salario = din*horas
+soma = p1+p2+p3+p4+p5
 
-print ("O seu salário esse mês é: R$ ", salario,".")
+if soma == 0:
+    print ('Você está livre')
+
+if soma > 5:
+    print ('Entrada inválida')
+
+if soma == 5:
+    print ('Você é o assassino')
+elif soma == 3 or soma == 4:
+    print ('Você é cúmplice')
+elif soma == 2:
+    print ('Você é suspeita')
---
+++ votos.py
@@ -0,0 +1,21 @@
+quant_eleitores = int(input("Digite a quantidade de eleitores: "))
+
+count_votos1 = 0
+count_votos2 = 0
+count_votos3 = 0
+
+for i in range (1, quant_eleitores):
+    voto = int(input("Digite o voto do eleitor %i: " %(i)))
+    if voto == 1:
+        count_votos1 += 1
+    elif  voto == 2:
+        count_votos2 += 1
+    elif voto == 3:
+        count_votos3 += 1
+    else:
+        print ("Entrada inválida")
+        exit()
+
+print ("%i voto(s) para o candidato 1.\
+    \n%i voto(s) para o candidato 2.\
+    \n%i voto(s) para o candidato 3." %(count_votos1, count_votos2, count_votos3))
