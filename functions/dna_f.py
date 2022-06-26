08ca007160426dbe08b31cf6cdf6b59c65b4f4ca
erikbayerlein <erik.bayer@icloud.com>
2022-06-26 18:23:27 -0300

    Update dna_f.py

--- functions/dna_f.py
+++ functions/dna_f.py
@@ -1,5 +1,5 @@
 # Faça uma função que recebe uma string que representa
-#  uma cadeia de DNA e gera a cadeia complementar. 
+# uma cadeia de DNA e gera a cadeia complementar. 
 # A entrada e saída de dados devem ser feitas pelo programa principal.
 #Entrada: AATCTGCAC 
 #Saída: TTAGACGTG
